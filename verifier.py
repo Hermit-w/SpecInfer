98f0376c12be02fd03daa60d43d7e42ae85ea375
Hermit <173976445@qq.com>
2025-08-24 19:00:22 +0800

    Add and implement some methods

--- verifier.py
+++ verifier.py
@@ -1,8 +1,13 @@
+import numpy as np
 import torch
 import transformers
+from transformers import LogitsProcessorList
+from transformers.modeling_outputs import CausalLMOutputWithPast
 
 from common import InputForCasualLm, OutputForCasualLm, synchronize_time
+import logging
 
+logger = logging.getLogger(__name__)
 
 class Verifier:
     def __init__(
@@ -13,14 +18,33 @@ class Verifier:
     ) -> None:
         self.model = model
         self.tokenizer = tokenizer
-        self.prompt_inputs = None
         self.processor = LogitsProcessorList()
 
-        self.set_prompt_time = 0
-        self.verify_times = []
-        self.prepare_input_time = 0
-        self.adjust_input_time = 0
-        self.benchmark_time = benchmark_time
+        self.verify_times: list[float] = []
+        self.prepare_input_time: float = 0.0
+        self.adjust_input_time: float = 0.0
+        self.benchmark_time: bool = benchmark_time
+
+    def compare_tokens(
+        self,
+        proposed_output: OutputForCasualLm,
+        verified_output: OutputForCasualLm
+    ) -> torch.Tensor:
+        if not proposed_output.output_ids.shape == \
+           verified_output.output_ids[:, :-1].shape:
+            logger.error(
+                f"{proposed_output.output_ids.shape}, \
+                  {verified_output.output_ids[:, :-1].shape}"
+            )
+            raise ValueError("Not compatiable shape")
+
+        # a = [[1, 2, 3]], b = [[1, 2, 4]]
+        # ~(a == b): [[0, 0, 1]]
+        # after cumsum: [[0, 0, 1]]
+        # after < 1: [[1, 1, 0]]
+        n_matches = ((~(proposed_output.output_ids ==
+                     verified_output.output_ids[:, :-1])).cumsum(dim=-1) < 1).sum()
+        return verified_output.output_ids[:, :n_matches + 1], -1, -1
 
     def verify(
         self,
@@ -31,10 +55,12 @@ class Verifier:
         if self.benchmark_time:
             start = synchronize_time()
 
-        outputs = self.model(input_ids=input.input_ids,
-                            attention_mask=input.attention_mask,
-                            past_key_values=input.past_key_values,
-                            use_cache=True)
+        outputs: CausalLMOutputWithPast = self.model(
+            input_ids=input.input_ids,
+            attention_mask=input.attention_mask,
+            past_key_values=input.past_key_values,
+            use_cache=True
+        )
 
         next_token_scores = self.processor(input.input_ids, outputs.logits)
         generated_len = propose_len + 1
@@ -48,8 +74,11 @@ class Verifier:
         return OutputForCasualLm(generated_len, None, logits.squeeze(0),
                               sample_method(logits.squeeze(0)), outputs.past_key_values)
 
-    def prepare_input(self, proposer_output: OutputForCasualLm,
-                      verifier_input: InputForCasualLm) -> InputForCasualLm:
+    def prepare_input(
+        self,
+        proposer_output: OutputForCasualLm,
+        verifier_input: InputForCasualLm
+    ) -> InputForCasualLm:
         if self.benchmark_time:
             start = synchronize_time()
 
@@ -77,10 +106,12 @@ class Verifier:
 
         return InputForCasualLm(input_ids, attention_mask, past_key_values)
 
-    def adjust_input(self,
-                     accept_token_ids: torch.Tensor,
-                     verifier_input: InputForCasualLm,
-                     verifier_output: OutputForCasualLm) -> InputForCasualLm:
+    def adjust_input(
+        self,
+        accept_token_ids: torch.Tensor,
+        verifier_input: InputForCasualLm,
+        verifier_output: OutputForCasualLm
+    ) -> InputForCasualLm:
         if self.benchmark_time:
             start = synchronize_time()
 
@@ -93,37 +124,27 @@ class Verifier:
         else:
             verifier_generated_len = verifier_output.past_key_values[0][0].shape[2] - (
                 verifier_output.generated_len - 1) + n_matches
-            if self.is_encoder_decoder:
-                verifier_key_values = crop_past_key_values_seq2seq(
-                    verifier_output.past_key_values, verifier_generated_len - 1)
-            else:
-                verifier_key_values = crop_past_key_values(
-                    verifier_output.past_key_values, verifier_generated_len - 1)
 
-                verifier_attn_masks = verifier_input.attention_mask[:,
-                                                                    :verifier_generated_len]
-                if verifier_attn_masks.shape[1] < verifier_generated_len:
-                    verifier_attn_masks = torch.cat([verifier_attn_masks,
-                                                    torch.ones(verifier_attn_masks.shape[0], 1, dtype=torch.long, device="cuda")], dim=-1)
+            verifier_key_values = crop_past_key_values(
+                verifier_output.past_key_values, verifier_generated_len - 1)
+
+            verifier_attn_masks = verifier_input.attention_mask[:,
+                                                                :verifier_generated_len]
+            if verifier_attn_masks.shape[1] < verifier_generated_len:
+                verifier_attn_masks = torch.cat([verifier_attn_masks,
+                                                torch.ones(verifier_attn_masks.shape[0], 1, dtype=torch.long, device="cuda")], dim=-1)
 
         if self.benchmark_time:
             self.adjust_input_time += synchronize_time() - start
-
-        if self.is_encoder_decoder:
-            return InputForCasualLm(verifier_input.input_ids,
-                                 verifier_input.attention_mask,
-                                 verifier_key_values,
-                                 verifier_input.labels,
-                                 accept_token_ids[:, -1])
-        else:
-            return InputForCasualLm(accept_token_ids[:, -1],
-                                 verifier_attn_masks,
-                                 verifier_key_values)
+        return InputForCasualLm(
+            accept_token_ids[:, -1],
+            verifier_attn_masks,
+            verifier_key_values
+        )
 
     def print_time(self):
         if self.benchmark_time:
             print(f"[Verifier] prompt phase: {self.verify_times[0]}, "
                   f"decode phase: {np.median(self.verify_times[1:])}, ",
-                  f"set prompt time: {self.set_prompt_time}, ",
                   f"adjust time: {self.adjust_input_time}, ",
                   f"prepare input time: {self.prepare_input_time}")
